32c2d8640ea0804cd20928d9a6e24d6c00de13fe
Aline Bertolazo dos Santos <aline16bertolazo04@gmail.com>
2024-09-05 14:19:38 -0300

    Adicionando funcionalidades JavaScript

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('Rebeca Andrade');
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById('resultados-pesquisa');
+
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = '';
+
+    // Itera sobre cada dado na lista de dados
+    for (let dado of dados) {
+        // Cria uma nova div para cada resultado, formatando-a como HTML
+        resultados += `<div class="item-resultado">
+            <h2>
+                <a href="#" target="_blank"> ${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao} </p>
+            <a href=${dado.link} target="_blank" >Mais Informações</a>
+        </div>`;
+    }
+
+    // Atribui a string com os resultados ao conteúdo HTML da seção
+    section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: 'Rayssa Leal',
+        descricao:
+            'Rayssa Leal, conhecida como "Fadinha", é uma skatista profissional brasileira que se tornou a mais jovem medalhista olímpica da história do Brasil. Com seu talento e carisma, ela inspirou uma nova geração de skatistas.',
+        link: 'https://pt.wikipedia.org/wiki/Rayssa_Leal',
+    },
+    {
+        titulo: 'Beatriz Souza',
+        descricao:
+            'Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024, tornando-se um dos principais nomes do judô brasileiro.',
+        link: 'https://pt.wikipedia.org/wiki/Beatriz_Souza',
+    },
+    {
+        titulo: 'Rebeca Andrade',
+        descricao:
+            'Rebeca Andrade é uma ginasta artística brasileira que conquistou diversas medalhas em competições nacionais e internacionais, incluindo ouro nos Jogos Olímpicos de Paris 2024.',
+        link: 'https://pt.wikipedia.org/wiki/Rebeca_Andrade',
+    },
+];
